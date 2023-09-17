a1798b3ac10019873a4ea1caf1f24e19c40d0b01
Maria-Bianca Militaru <bianca.militaru@yahoo.com>
2023-09-17 19:39:50 +0200

    Fetching some data

--- src/index.js
+++ src/index.js
@@ -1,4 +1,25 @@
 import _ from 'lodash';
+import model, { cloud, precipitation, temperature, wind } from '../model/model';
+
+function displayData(id){
+    var x = document.getElementById(id);
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none"; 
+    }
+}
+
+async function fetchData(city){
+    try {
+        const response = await fetch('http://localhost:8080/data/Horsens')
+        if (!response.ok) throw response.statusText
+        const data = await response.json()
+        return data;
+    } catch (e) {
+        theView.displayError(e)
+    }
+}
 
 function title(){
     const element = document.createElement('h1');
@@ -15,12 +36,8 @@ function componentHorsens(){
     const button = document.createElement('button');
     button.id = "buttonHorsens";
     button.onclick = () => {
-        var x = document.getElementById("tableHorsens");
-        if (x.style.display === "none") {
-            x.style.display = "block";
-        } else {
-            x.style.display = "none"; 
-        }
+        displayData("tableHorsens");
+        fetchData();
     }
     button.innerHTML = _.join(['Horsens']);
     div.appendChild(button);    
@@ -41,12 +58,7 @@ function componentAarhus(){
     const button = document.createElement('button');
     button.id = "buttonAarhus";
     button.onclick = () => {
-        var x = document.getElementById("tableAarhus");
-        if (x.style.display === "none") {
-            x.style.display = "block";
-        } else {
-            x.style.display = "none"; 
-        }
+        displayData("tableAarhus");
     }
     button.innerHTML = _.join(['Aarhus']);
     div.appendChild(button);    
@@ -67,12 +79,7 @@ function componentCopenhagen(){
     const button = document.createElement('button');
     button.id = "buttonCopenhagen";
     button.onclick = () => {
-        var x = document.getElementById("tableCopenhagen");
-        if (x.style.display === "none") {
-            x.style.display = "block";
-        } else {
-            x.style.display = "none"; 
-        }
+        displayData("tableCopenhagen");
     }
     button.innerHTML = _.join(['Copenhagen']);
     div.appendChild(button);    
